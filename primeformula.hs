658c922781186074ac9b3a5527161c9e9b55f75b
Alan Hawkins <hawk.alan@gmail.com>
2014-08-28 13:25:29 +1000

    Create primeformula.hs

---
+++ primeformula.hs
@@ -0,0 +1,18 @@
+main = do
+       print (primeFactors 53)
+       print (
+              (cos 
+                (pi *  
+                  ((fac (7-1) + 1)/7)
+                )
+              )^2
+             )
+
+fac n = product [1..n]
+
+primeFactors n = primeFactors' n 2
+  where
+    primeFactors' 1 _ = []
+    primeFactors' n f
+      | n `mod` f == 0 = f : primeFactors' (n `div` f) f
+      | otherwise      = primeFactors' n (f + 1)
