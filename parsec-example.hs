793113b06c5c79e5236569a4e64e62ff47fc98db
Alan Hawkins <alanpika_au@yahoo.com>
2014-10-13 19:44:43 +1100

    Create parsec-example.hs

---
+++ parsec-example.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+import Text.ParserCombinators.Parsec
+import Control.Monad
+ 
+arbitaryCoin = do 
+ x <- string "("
+ y <- manyTill ( (try arbitaryCoin) <|>  (anyChar >>= return . (:[])) ) (string ")") 
+ return (x++(concat y)++")")
+  
+parser = do
+  b <- many1 arbitaryCoin 
+  eof
+  return (concat b)
+  
+main = print $ map (parse parser "") [ "((((dd))))" , "(((())))" ,"()()"]
+fail =  print $ map (parse parser "") ["(((()))"]
