7f5f2d9d3f86b6bf11abc790b0c73b193db21620
Alan Hawkins <alanpika_au@yahoo.com>
2014-10-28 16:22:04 +1100

    Create Applicaty.hs

---
+++ Applicaty.hs
@@ -0,0 +1,11 @@
+
+import Control.Monad
+
+class (Monad m,Functor m) => Applicaty m where
+  app :: m (a->b) -> m a -> m b
+  app x y = join (fmap (\z -> fmap (z$) y) x)
+  purey :: a -> m a
+  purey = return
+
+instance Applicaty [] where
+                                                    
