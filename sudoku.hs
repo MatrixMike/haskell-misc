eed742a553b7d9eabd1ad89e19c6d99b47e59fdc
Alan Hawkins <alanpika_au@yahoo.com>
2014-12-08 15:41:06 +1100

    Update sudoku.hs

--- sudoku.hs
+++ sudoku.hs
@@ -1,49 +1,144 @@
+{-# Language NoMonomorphismRestriction #-}
 import Data.List.Split
 import Data.List
---import Data.Monoid
 import Control.Monad
-
-sudoku = [
-        8, 5, 0, 0, 0, 2, 4, 0, 0,
-        7, 2, 0, 0, 0, 0, 0, 0, 9,
-        0, 0, 4, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 1, 0, 7, 0, 0, 2,
-        3, 0, 5, 0, 0, 0, 9, 0, 0,
-        0, 4, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 8, 0, 0, 7, 0,
-        0, 1, 7, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 3, 6, 0, 4, 0]
-
-
-sudokuBad = [
-        2, 5, 0, 0, 0, 2, 4, 0, 0,
-        7, 2, 0, 0, 0, 0, 0, 0, 9,
-        0, 0, 4, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 1, 0, 7, 0, 0, 2,
-        3, 0, 5, 0, 0, 0, 9, 0, 0,
-        0, 4, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 8, 0, 0, 7, 0,
-        0, 1, 7, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 3, 6, 0, 4, 0]
+import Data.Maybe
+import Control.Monad.ST
+import Data.STRef
 
 horizontals = chunksOf 9 
-
 verticals = transpose . chunksOf 9
+squares =  
+       concat 
+     . map (map concat) 
+     . map (chunksOf 3) 
+     . transpose 
+     . map (chunksOf 3) 
+     . horizontals 
 
-validArea xs = length (nub (filterZeros xs)) == length (filterZeros xs)
- 
-filterZeros = filter (not . (==0))
+checkAreas f xs = all (all (==True)) (checkAreas' f xs)
 
-totalyValidArea xs = sort xs == [1..9]
+checkAreas' f xs = map (map f) [verticals xs,horizontals xs,squares xs]
 
-squares xs= concat $ map (map concat) $  map (chunksOf 3) (transpose ((map (chunksOf 3) (horizontals xs))))
+checkValidSudoku = checkAreas validArea 
 
-checkAreas f xs =
-  all (all (==True))
-   (map (map f) [verticals xs,horizontals xs,squares xs])
+validArea xs = length (nub xs) == length xs
 
-checkValidSudoku xs = checkAreas validArea xs
+if' True x y = x 
+if' False x y = y
+
+predicateToMaybe p v = case p v of 
+   True -> Just v
+   False -> Nothing
+   
+maybeToEither (Just x) = Left x
+maybeToEither Nothing = Right ()
+
+solver = head . solver'1
+
+solver'1 xs = runST $ do
+  result <- newSTRef []
+  forM xs $ \v -> do
+
+  return (filter checkValidSudoku $ sequence $ solver'2 xs)
+
+solver'2 = map (either ((:[]) . id) (const [1..9])) . solver'3
+solver'3 = map maybeToEither . solver'4
+solver'4 = map (predicateToMaybe (not . (==0)))
+
+sudokuMissing8 = concat [
+	 [0,0,0,0,0,0,0,0,1]
+	,[9,3,8,1,2,6,5,4,7]
+	,[4,1,2,7,9,5,3,6,8]
+	,[6,8,9,3,7,2,1,5,4]
+	,[7,4,3,6,5,1,8,2,9]
+	,[1,2,5,8,4,9,6,7,3]
+	,[2,5,4,9,3,8,7,1,6]
+	,[3,7,1,2,6,4,9,8,5]
+	,[8,9,6,5,1,7,4,3,2]
+  ]
+
+sudokuMissing7 = concat [
+	 [0,0,0,0,0,0,0,9,1]
+	,[9,3,8,1,2,6,5,4,7]
+	,[4,1,2,7,9,5,3,6,8]
+	,[6,8,9,3,7,2,1,5,4]
+	,[7,4,3,6,5,1,8,2,9]
+	,[1,2,5,8,4,9,6,7,3]
+	,[2,5,4,9,3,8,7,1,6]
+	,[3,7,1,2,6,4,9,8,5]
+	,[8,9,6,5,1,7,4,3,2]
+  ]
+
+sudokuMissing6 = concat [
+	 [0,0,0,0,0,0,2,9,1]
+	,[9,3,8,1,2,6,5,4,7]
+	,[4,1,2,7,9,5,3,6,8]
+	,[6,8,9,3,7,2,1,5,4]
+	,[7,4,3,6,5,1,8,2,9]
+	,[1,2,5,8,4,9,6,7,3]
+	,[2,5,4,9,3,8,7,1,6]
+	,[3,7,1,2,6,4,9,8,5]
+	,[8,9,6,5,1,7,4,3,2]
+  ]
 
 
+sudokuMissing5 = concat [
+	 [0,0,0,0,0,3,2,9,1]
+	,[9,3,8,1,2,6,5,4,7]
+	,[4,1,2,7,9,5,3,6,8]
+	,[6,8,9,3,7,2,1,5,4]
+	,[7,4,3,6,5,1,8,2,9]
+	,[1,2,5,8,4,9,6,7,3]
+	,[2,5,4,9,3,8,7,1,6]
+	,[3,7,1,2,6,4,9,8,5]
+	,[8,9,6,5,1,7,4,3,2]
+  ]
 
+sudokuMissing3 = concat [
+	 [0,0,0,4,8,3,2,9,1]
+	,[9,3,8,1,2,6,5,4,7]
+	,[4,1,2,7,9,5,3,6,8]
+	,[6,8,9,3,7,2,1,5,4]
+	,[7,4,3,6,5,1,8,2,9]
+	,[1,2,5,8,4,9,6,7,3]
+	,[2,5,4,9,3,8,7,1,6]
+	,[3,7,1,2,6,4,9,8,5]
+	,[8,9,6,5,1,7,4,3,2]
+  ]
 
+sudokuMissing2 = concat [
+	 [0,0,7,4,8,3,2,9,1]
+	,[9,3,8,1,2,6,5,4,7]
+	,[4,1,2,7,9,5,3,6,8]
+	,[6,8,9,3,7,2,1,5,4]
+	,[7,4,3,6,5,1,8,2,9]
+	,[1,2,5,8,4,9,6,7,3]
+	,[2,5,4,9,3,8,7,1,6]
+	,[3,7,1,2,6,4,9,8,5]
+	,[8,9,6,5,1,7,4,3,2]
+  ]
+
+sudokuMissing1 = concat [
+	 [0,6,7,4,8,3,2,9,1]
+	,[9,3,8,1,2,6,5,4,7]
+	,[4,1,2,7,9,5,3,6,8]
+	,[6,8,9,3,7,2,1,5,4]
+	,[7,4,3,6,5,1,8,2,9]
+	,[1,2,5,8,4,9,6,7,3]
+	,[2,5,4,9,3,8,7,1,6]
+	,[3,7,1,2,6,4,9,8,5]
+	,[8,9,6,5,1,7,4,3,2]
+  ]
+
+sudokuCorrect = concat [
+	 [5,6,7,4,8,3,2,9,1]
+	,[9,3,8,1,2,6,5,4,7]
+	,[4,1,2,7,9,5,3,6,8]
+	,[6,8,9,3,7,2,1,5,4]
+	,[7,4,3,6,5,1,8,2,9]
+	,[1,2,5,8,4,9,6,7,3]
+	,[2,5,4,9,3,8,7,1,6]
+	,[3,7,1,2,6,4,9,8,5]
+	,[8,9,6,5,1,7,4,3,2]
+  ]
