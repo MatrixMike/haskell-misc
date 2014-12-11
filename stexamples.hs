87cf58c2ff64a8d03803f429ad7ece059dc51814
Alan Hawkins <alanpika_au@yahoo.com>
2014-12-11 12:51:33 +1100

    Create stexamples.hs

---
+++ stexamples.hs
@@ -0,0 +1,27 @@
+import Data.STRef
+import Control.Monad.ST
+import Control.Monad
+
+sumUsingST xs = runST $ do
+                  counter <- newSTRef 0
+                  forM_ xs (\v -> modifySTRef counter (+v) )
+                  readSTRef counter 
+
+
+    
+
+sequenceUsingST xs = runST $ do
+  result <- newSTRef []
+  case xs of 
+   (a@(_:_):b) -> forM_ a $ \v -> do
+	let rest = sequenceUsingST b
+	modifySTRef result (++(map (v:) rest))
+   _ -> writeSTRef result [[]]
+  readSTRef result 
+
+sequenceUsingST2 xss = runST $ do
+  results <- newSTRef [[]]
+  forM xss $ \xs -> do
+    results' <- readIORef results
+    writeSTRef (map (\x->++[x]) results') results
+  readSTRef results
