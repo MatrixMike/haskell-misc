a1b661d1da5af8d6557abddffdef375e483ffbd6
Alan Hawkins <alanpika_au@yahoo.com>
2014-12-23 14:31:26 +1100

    Update nibList.hs

--- nibList.hs
+++ nibList.hs
@@ -4,11 +4,10 @@
 import Data.List
 
 data NibList a = Node a (NibList a) | Nib (NibList a) 
- deriving (Eq, Read,Ord)
+  deriving (Eq, Read,Ord)
 
 instance Show a => Show (NibList a) where  
-  show (Nib _) = "Nib"
-  show a@(Node _ _) = showNib False (lengthNib a+1) a
+  show a = showNib False (lengthNib a+1) a
 
 showNib _ 0 _ = ""
 showNib b x (Node a nl) = if b then "("++t++")" else t
@@ -16,14 +15,31 @@ showNib b x (Node a nl) = if b then "("++t++")" else t
 showNib b x (Nib nl) = if b then  "("++t++")" else t
   where t = "Nib" ++ showNib True (x-1) nl  
 
-lengthNib  (Node a nl) = lengthNib nl
-lengthNib  (Nib nl) = lengthNib' nl
-lengthNib' (Node _ nl) = 1 + lengthNib' nl
-lengthNib' (Nib _) = 0
+lengthNib       (Nib nl) = lengthNib'' nl
+lengthNib             nl = lengthNib' nl
+lengthNib'   (Node a nl) = lengthNib' nl
+lengthNib'      (Nib nl) = lengthNib'' nl
+lengthNib''  (Node _ nl) = 1 + lengthNib'' nl
+lengthNib''      (Nib _) = 0
 
-nibList = Node 1 (Node 2 (Nib nibList))
-nibList2 = Node 1 (Node 2 (Nib (Node 3 nibList)))
+applyNTimes f n x = iterate f x !! n
+
+tailNib (Nib nl) = nl
+tailNib (Node _ nl) = nl
 
 emptyNib = Nib emptyNib :: NibList Int
 
 main = print emptyNib
+
+nibList = Node 1 (Node 2 (Nib nibList))
+nibList2 = Node 1 (Node 2 (Nib (Node 3 nibList2)))
+nibList3 = Node 1 (Node 2 (Node 8 (Nib nibList3)))
+nibList4 = Nib (Node 2 (Node 8 nibList4))
+
+tests = do
+        let a = Nib (a) in print $ (lengthNib a == 0) == True
+        let a = Nib (Node 1 a) in print $ (lengthNib a == 1) == True
+        let a = Node 1 (Nib (Node 2 a)) in print $ (lengthNib a == 2) == True
+
+
+
