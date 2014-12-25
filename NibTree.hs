67acea0ef2202fddfbc323c6f7a17e9a0e57280d
Alan Hawkins <alanpika_au@yahoo.com>
2014-12-25 22:34:23 +1100

    Update NibTree.hs

--- NibTree.hs
+++ NibTree.hs
@@ -1,3 +1,9 @@
 
 
-data NibTree a = Nib (NibTree a) | Branch a (NibTree) | Leaf a 
+data NibTree a = Nib (NibTree a) | Branch a (NibTree) | Leaf a
+
+depth_first = undefined
+pre_order = undefined
+post_order = undefined
+in_order = undefined
+reverse_post_order = undefined
