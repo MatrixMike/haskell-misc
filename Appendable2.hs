56a3e4cc2b76e6b9359c4ca9419c8f5653f01aa5
Alan Hawkins <alanpika_au@yahoo.com>
2014-12-28 21:54:28 +1100

    Update Appendable2.hs

--- Appendable2.hs
+++ Appendable2.hs
@@ -1,11 +1,24 @@
 data BinaryTree a = Branch (BinaryTree a) (BinaryTree a) | Leaf a
  deriving (Show,Ord,Eq)
 
-prepend a b = concatenateTrees (Leaf a) b
+prepend a b = concatenateTrees (Leaf a) b 
 append a b = concatenateTrees b (Leaf a)
 
 concatenateTrees a b = Branch a b
 
-fromList (x:xs) = foldr prepend (Leaf x) xs
+fromList (x:xs) = foldl (flip append) (Leaf x) xs
 
 main = print $ (\x -> concatenateTrees x x) (fromList "ab")
+
+sumTree (Branch x y) = sumTree x + sumTree y 
+sumTree (Leaf x) = x
+
+eg2 = sumTree $ concatenateTrees (fromList [1,2]) (fromList [3,4])
+
+treeToList tree = (treeToList' tree) []
+treeToList' (Branch x y) = \z -> (treeToList' x) ((treeToList' y) z)
+treeToList' (Leaf x) = \y -> x :y 
+
+
+
+
