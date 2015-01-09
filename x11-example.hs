306c1a031e1ed2dd1969f4d6cea7b5e301b1a0d7
Alan Hawkins <alanpika_au@yahoo.com>
2015-01-10 08:42:33 +1100

    Create x11-example.hs

---
+++ x11-example.hs
@@ -0,0 +1,47 @@
+import Graphics.X11.Xlib
+import System.Exit (exitWith, ExitCode(..))
+import Control.Concurrent (threadDelay)
+import Control.Monad
+import Graphics.X11.Xlib.Extras
+import Data.Bits
+ 
+main :: IO ()
+main = do
+ dpy <- openDisplay ""
+ let dflt = defaultScreen dpy
+     border = blackPixel dpy dflt
+     background = whitePixel dpy dflt
+     scr = defaultScreenOfDisplay dpy
+     visual = defaultVisualOfScreen scr
+     attrmask = 0
+ rw <- rootWindow dpy dflt
+ win <- allocaSetWindowAttributes $ \attributes -> do
+         createWindow dpy rw 0 0 200 200 0 (defaultDepthOfScreen scr) inputOutput visual attrmask attributes
+ setTextProperty dpy win "Hello World" wM_NAME
+ gc <- createGC dpy win
+ mapWindow dpy win
+ forM_ (concat $repeat $ [1..100]++[99,98..2]) $ \x -> do
+   threadDelay (10000)
+   clearWindow dpy win
+   drawInWin x dpy win gc
+   flush dpy
+
+ {-
+ freeGC dpy gc
+ freePixmap dpy p
+ exitWith ExitSuccess
+ -}
+
+drawInWin x dpy win gc = do
+ bgcolor <- initColor dpy "green"
+ fgcolor <- initColor dpy "blue"
+ setForeground dpy gc bgcolor
+ fillRectangle dpy win gc 0 0 200 200
+ setForeground dpy gc fgcolor
+ fillRectangle dpy win gc ((2+fromIntegral x)) 2 96 96
+ 
+initColor :: Display -> String -> IO Pixel
+initColor dpy color = do
+ let colormap = defaultColormap dpy (defaultScreen dpy)
+ (apros,real) <- allocNamedColor dpy colormap color
+ return $ color_pixel apros
