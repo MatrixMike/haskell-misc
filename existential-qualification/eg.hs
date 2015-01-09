8d5990123daaa4ce8afa51459fc315594d93f5a0
alan hawkins <hawk.alan@gmail.com>
2015-01-10 08:33:09 +1100

    added qualification

---
+++ existential-qualification/eg.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE ExistentialQuantification #-} 
+
+
+data ShowBox = forall s. Show s => SB s
+ 
+heteroList :: [ShowBox]
+heteroList = [SB (), SB 5, SB True]
+
+
+instance Show ShowBox where
+ show (SB s) = show s        -- (*) see the comment in the text below
+ 
+f :: [ShowBox] -> IO ()
+f xs = mapM_ print xs
+
+main = f heteroList
+
---
+++ existential-qualification/eg2.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE ExistentialQuantification #-} 
+
+import Data.Typeable
+
+data TypeableBox = forall s. Typeable s => TB s
+ 
+heteroList :: [TypeableBox]
+heteroList = [TB True,TB (2::Int),TB False,TB (),TB (4::Int)]
+
+isInteger :: (Typeable a) => a -> Bool
+isInteger n = typeOf n == typeOf (1::Int)
+
+main = print $ length $ filter id $ map (\(TB x)-> isInteger x) heteroList
+
