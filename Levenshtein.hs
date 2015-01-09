2ad2e6441a136f3905d386a952e34de5348af4a7
Alan Hawkins <alanpika_au@yahoo.com>
2015-01-10 08:20:33 +1100

    Create Levenshtein.hs

---
+++ Levenshtein.hs
@@ -0,0 +1,11 @@
+
+levenshtein s1 s2 = levenshtein' 0 s1 s2
+levenshtein' x s1 "" = x + length s1
+levenshtein' x "" s2 = x + length s2
+levenshtein' x s1 s2 = minimum ([tryDelete,tryInsert,trySubstitute]++trySkip)
+  where tryDelete = levenshtein' (x+1) (drop 1 s1) s2  
+        tryInsert = levenshtein' (x+1) (take 1 s2 ++ drop 1 s1) (drop 1 s2)
+        trySubstitute = levenshtein' (x+1) (drop 1 s1) (drop 1 s2)
+        trySkip = if take 1 s1 == take 1 s2 then [levenshtein' x (drop 1 s1) (drop 1 s2)] else []
+        
+main = print (levenshtein "0a0b0" "ab")
