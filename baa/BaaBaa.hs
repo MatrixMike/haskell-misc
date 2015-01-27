7ff17662e68c98467538d7a787e1b3fc7d3decae
Alan Hawkins <alanpika_au@yahoo.com>
2015-01-27 16:00:00 +1100

    Create BaaBaa.hs

---
+++ baa/BaaBaa.hs
@@ -0,0 +1,31 @@
+--import Midi
+import Wav
+import Music
+import Data.ByteString as BS
+--import Midi
+
+main = put_music "baaBaa.wav" baaBaa
+
+--main :: IO ()
+--main = BS.writeFile "baaBaa.mid" (make_music baaBaa)
+
+intro :: Music
+intro  = Sequence [ C, C, G, G ]
+
+run :: Music
+run    = Sequence [ A, B, C, A ]
+
+middle :: Music
+middle = Sequence [ F, F, E, E ]
+
+run2 :: Music
+run2   = Sequence [ D, C, D, E ]
+
+baaBaa :: Music
+baaBaa = Sequence [ Longer 2  intro
+                  , Higher 12 run
+                  , Longer 4  G
+                  , Longer 2  middle
+                  , run2
+                  , Longer 4  C ]
+           
