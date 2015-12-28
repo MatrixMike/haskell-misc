24efc12cbc73f3fc5f86a949fb285efc40f8ebc2
root <hawk.alan@gmail.com>
2015-12-28 08:58:57 +0000

    add

---
+++ treepend.hs
@@ -0,0 +1,20 @@
+
+import Data.Tree
+
+
+{-
+listToTree xs@(x:_) = Just v
+  where [v] = listToTree' xs
+listToTree [] = Nothing
+listToTree' (x:xs) = [Node x (listToTree' xs)]
+listToTree' [] = []
+-}
+
+
+
+
+listToTree xs@(x:_) = v
+  where [v] = listToTree' xs
+listToTree [] = error "Empty List"
+listToTree' (x:xs) = [Node x (listToTree' xs)]
+listToTree' [] = []
