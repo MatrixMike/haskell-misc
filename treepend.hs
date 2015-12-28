57a62e7d35321aa364ace4fd7a11b084e682d30d
root <hawk.alan@gmail.com>
2015-12-28 09:11:55 +0000

    add

--- treepend.hs
+++ treepend.hs
@@ -13,8 +13,13 @@ listToTree' [] = []
 
 
 
+{-
 listToTree xs@(x:_) = v
   where [v] = listToTree' xs
 listToTree [] = error "Empty List"
 listToTree' (x:xs) = [Node x (listToTree' xs)]
 listToTree' [] = []
+-}
+
+
+listToTree (x:xs) = foldl (\tree v-> Node v [tree]) (Node x []) (reverse xs)
