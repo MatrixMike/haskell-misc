c1e3cdfaa0e1bbae9bef3bda096aeee779ab9fbc
Alan Hawkins <hawk.alan@gmail.com>
2015-12-30 00:27:35 +1100

    mod

--- treepend.hs
+++ treepend.hs
@@ -2,41 +2,21 @@
 import Data.Tree
 
 
-{-
-listToTree xs@(x:_) = Just v
-  where [v] = listToTree' xs
-listToTree [] = Nothing
-listToTree' (x:xs) = [Node x (listToTree' xs)]
-listToTree' [] = []
--}
-
-
-
-
-{-
-listToTree xs@(x:_) = v
-  where [v] = listToTree' xs
-listToTree [] = error "Empty List"
-listToTree' (x:xs) = [Node x (listToTree' xs)]
-listToTree' [] = []
--}
-
 treePrepend tree@(Node v1 _) v2 = Node v2 [tree]
 
 listToTree xs = foldr (flip treePrepend) (Node (last xs) []) (init xs)
+
+
+listOverTree xs'@(x:xs) (Node x' ts) 
+  | (x == x') = if (x `elem` (map rootLabel ts)) then (Node x' (map (listOverTree xs) ts))
+                                                 else (Node x' ((listToTree xs):ts))
+  | otherwise = (Node x' ((listToTree xs'):ts))
+listOverTree [] tree = tree
+
+
 
 treeToList (Node x []) = [x] 
 treeToList (Node x xs) = x : concatMap treeToList xs
 
 
--- treePend (Node x xs) (Node x' xs) = if x == 
 
-
--- zipBy p xs ys = xs >>= \x -> return $ filter (p x) ys
-
-
-
-pairBy p xs = map (pairBy' p) (take (length xs) (iterate rotate xs))
-pairBy' p (x:xs) = pairBy'' p x xs
-pairBy'' p x xs = (x,filter (p x) xs)
-rotate (x:xs) = xs ++ [x]
