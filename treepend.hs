94895ba60ed6037a103d926f3937e5db4dcbe749
root <hawk.alan@gmail.com>
2015-12-29 10:39:46 +0000

    it

--- treepend.hs
+++ treepend.hs
@@ -21,5 +21,22 @@ listToTree' (x:xs) = [Node x (listToTree' xs)]
 listToTree' [] = []
 -}
 
+treePrepend tree@(Node v1 _) v2 = Node v2 [tree]
 
-listToTree (x:xs) = foldl (\tree v-> Node v [tree]) (Node x []) (reverse xs)
+listToTree xs = foldr (flip treePrepend) (Node (last xs) []) (init xs)
+
+treeToList (Node x []) = [x] 
+treeToList (Node x xs) = x : concatMap treeToList xs
+
+
+-- treePend (Node x xs) (Node x' xs) = if x == 
+
+
+-- zipBy p xs ys = xs >>= \x -> return $ filter (p x) ys
+
+
+
+pairBy p xs = map (pairBy' p) (take (length xs) (iterate rotate xs))
+pairBy' p (x:xs) = pairBy'' p x xs
+pairBy'' p x xs = (x,filter (p x) xs)
+rotate (x:xs) = xs ++ [x]
