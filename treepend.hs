5f3897a5f7e6944c451b93b7645def1457ea233c
Alan Hawkins <hawk.alan@gmail.com>
2015-12-30 11:19:52 +1100

    simplified function

--- treepend.hs
+++ treepend.hs
@@ -1,6 +1,5 @@
 
 import Data.Tree
-import Control.Monad.Writer
  
 treePrepend tree@(Node v1 _) v2 = Node v2 [tree]
 
@@ -13,14 +12,12 @@ listOverTree xs'@(x:xs) (Node x' ts)
   | otherwise = (Node x' ((listToTree xs'):ts))
 listOverTree [] tree = tree
 
-treeToLists tree = map reverse $ treeToLists' [] tree
-treeToLists' buf (Node x []) = [x:buf]
-treeToLists' buf (Node x xs) = concatMap (treeToLists' (x:buf)) xs
+treeToLists (Node x []) = [[x]]
+treeToLists (Node x xs) = concatMap ((\(y:ys)->([x]++y):ys) . treeToLists) xs
 
 treeMerge tree1 tree2 = foldr listOverTree tree1 (treeToLists tree2)
 
 tree1 = (Node 1 [Node 2 [],Node 3 [Node 4 []]])
-
 tree2 = (Node 1 [Node 2 [Node 20 [Node 200 []] , Node 22 [Node 220 []] ]])
 
 main = print $ treeMerge tree1 tree2
