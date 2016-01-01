81d2b1fa04f27ce663f41e67860e42c66fa861e9
root <hawk.alan@gmail.com>
2016-01-01 10:25:05 +0000

    uniplatest

--- uniplateTest.hs
+++ uniplateTest.hs
@@ -1,20 +1,34 @@
 --import Data.Generics.Uniplate.Direct 
 
-import Data.Generics.PlateDirect
+--import Data.Generics.PlateDirect
+import Data.Generics.Uniplate.Direct
+import Data.Generics.Uniplate.Operations
+import Data.Char
+import Control.Monad.IO.Class
 
 data MyList = MyCons Char MyList 
             | MyEnd
   deriving Show
 
+example = MyCons 'a' (MyCons 'b' (MyCons 'c' MyEnd))
+
+former (MyCons a rest) = MyCons (toUpper a) (former rest)
+former x = x
+
+eg = transform former example
+eg2 = transformM (\x -> print 1 >> return x) example
+
+instance Uniplate MyList where 
+  uniplate (MyCons str rest) = plate (MyCons str) |* rest
+  uniplate MyEnd =  plate MyEnd
 
 
-example = MyCons 'a' (MyCons 'b' MyEnd)
-eg =  descend id example
 
-
+{-
 instance Uniplate MyList where 
   uniplate (MyCons str rest) = (Two Zero (One rest),\(Two Zero (One rest)) -> (const $ MyCons str) Zero rest)
   uniplate MyEnd =  (Zero, const MyEnd)
+-}
   
 
 
