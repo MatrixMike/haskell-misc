33a60cf3ecf65ad1c333e89a3b5ed83155843da8
root <hawk.alan@gmail.com>
2016-01-31 08:30:45 +0000

    updated xml parser to support tags without content

--- xmlparser.hs
+++ xmlparser.hs
@@ -25,32 +25,28 @@ attr = do
 
 many1Till p e = do
        notFollowedBy e
-       x <- p 
-       xs <- manyTill p e
+       x <- p
+       xs <- manyTill p (try e)
        return (x:xs)
 
 many1Till' p e = do
                  xs <- many1Till p (lookAhead e)
                  y <- e
                  return (xs,y)
-       
 
 openTag = do
- x <- string "<"
+ string "<"
  skipMany space
  many1Till' anyChar $ do
-    attrs <- spaceOut $ try (many attr)
-    y <- string ">"
-    return attrs
+  attrs <- spaceOut $ try (many attr)
+  string ">"
+  return attrs
 
 closeTag str = do
  x <- string "</"
  y <- string str
  z <- (string ">")
  return y
-
-plus :: (Num number) => number -> number
-plus = (+1)
 
 sparse p t = parse p "" t
 sameConstructor (Left _) (Left _) = True
@@ -60,21 +56,33 @@ sameConstructor _ _ = False
 groupEithers :: [Either a b] -> [Either [a] [b]]
 groupEithers xs = map (\xs@(x:_) -> either (const (Left (lefts xs))) (const (Right (rights xs))) x)  (groupBy sameConstructor xs)
  
-wholeTag = do
+tagWithoutContent = do
+  (x,a) <- tagWithoutContent'
+  return (Tag a x [])
+
+  
+tagWithoutContent' = do 
+ string "<"
+ skipMany space
+ many1Till' anyChar $ do
+  attrs <- spaceOut $ try (many (try attr))
+  string "/>"
+  return attrs
+
+closedTag = do
  (x,a) <- openTag
- y <- manyTill (
-                   try (wholeTag >>= return . Left) 
-                   <|> 
-				   ( (noneOf "<>") >>= return . Right) ) 
-			   (try $ closeTag x)
+ y <- manyTill ( try (closedTag >>= return . Left) <|> ((noneOf "<>") >>= return . Right)) (try $ closeTag x)
  let s = map (either (TagInner . head) TagString) (groupEithers y)
  return (Tag a x s)
   
-parser = do
-  b <- wholeTag
-  eof
-  return (b)
-  
+fragmentParse = do
+  b <- closedTag  
+  return b
+
+fullParser =do 
+            x <- try tagWithoutContent <|> fragmentParse
+            eof 
+            return x
   
 showAttribute (x,y) = x++"="++"\""++y++"\""
 showAttributes atts = concat $ intersperse " " $ map showAttribute atts
@@ -91,9 +99,7 @@ showTagInnards innard = case innard of
   (TagInner inner) -> showTag inner
  
 main = do 
-       let str = "<one green='blue'>five<two>to you</two></one>"
+       let str = "<one green=\"blue\">five<two>to you</two></one>"
        print str
-       let [Right v] =  map (parse parser "") [ str]
-       print v
-       putStrLn (showTag v)
-   
+       let rs =  map (parse fullParser "") [str,"<single/>"]
+       mapM_ (either (const (print "fail")) (putStrLn . showTag)) rs
