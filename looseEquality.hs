ad1d84715b580c18a7e97f8bb03109f38cd0c683
Alan Hawkins <alanpika_au@yahoo.com>
2016-06-13 11:57:32 +1000

    Update looseEquality.hs

--- looseEquality.hs
+++ looseEquality.hs
@@ -1,7 +1,8 @@
-
-
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverlappingInstances #-}
+{-# LANGUAGE IncoherentInstances #-}
+
 
 import Prelude hiding ((==))
 import qualified Prelude as P 
@@ -12,10 +13,15 @@ class MyEq a b where
 instance ( Num a, Eq a) => MyEq a [Char] where
   0 == "" = True
   _ == _ = False 
+
+instance ( Real a, Real b) => MyEq a b where
+  a == b = realToFrac a P.== realToFrac b
   
-  
+
 {-
 0 == "" 
-
 => True
+
+0 == 1.0
+=> False
 -}
