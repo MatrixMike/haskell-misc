ce69fd751629f25ddd474292042d2223b0f03686
Alan Hawkins <alanpika_au@yahoo.com>
2017-01-02 18:53:14 +1100

    Update pipes_pi.hs

--- pipes_pi.hs
+++ pipes_pi.hs
@@ -8,6 +8,9 @@ import Control.Monad.Identity
 
 main = newStdGen >>= Prelude.print . w
 
-w a = (\x ->  (*4) $ (\y -> (fromIntegral y )/(fromIntegral x) ) $  runIdentity $  Pipes.Prelude.length (  f a >-> forever (do {x <- await ; y <- await ; yield (x,y) }) >-> Pipes.Prelude.take x >-> Pipes.Prelude.filter (\(x,y) -> x*x + y*y < 1.0))) 100000
+w a =    (\x ->  (*4)
+       $ (\y -> (fromIntegral y )/(fromIntegral x) )
+       $  runIdentity
+       $  Pipes.Prelude.length (  g a >-> forever (do {x <- await ; y <- await ; yield (x,y) }) >-> Pipes.Prelude.take x >-> Pipes.Prelude.filter (\(x,y) -> x*x + y*y < 1.0))) 100000
 
-f x = (\(a,b) -> yield (a::Float) >> f b) (System.Random.random x)
+g = unfoldr (\s-> return (Right ((\(x,y)->(x::Float,y)) (random s))))
