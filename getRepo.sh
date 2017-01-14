00aee519be2a474e1d0bc9e76aa91fffb35831e2
Alan Hawkins <alanpika_au@yahoo.com>
2017-01-15 00:26:22 +1100

    Create getRepo.sh

---
+++ getRepo.sh
@@ -0,0 +1,22 @@
+
+[ -d $1* ] || cabal get $1
+cd $1*
+
+function f1(){
+cat  <<EOF  | runhaskell
+
+import Distribution.PackageDescription.Parse 
+import Distribution.PackageDescription 
+import Data.Maybe
+
+main =  fmap ( (\(ParseOk _ x) -> x) .  (fmap ( fromJust .  repoLocation . head  .  sourceRepos .   packageDescription) ) . parsePackageDescription) (readFile "lens.cabal") >>= putStrLn
+
+EOF
+
+
+
+}
+
+repo=$(f1 $1)
+
+git clone $repo ${1}_repo
