4179435915eccc3b3a896f978a7c74dbb52236b9
Alan Hawkins <alanpika_au@yahoo.com>
2017-01-15 14:32:43 +1100

    Update setRepo.hs

--- cabal-utils/setRepo.hs
+++ cabal-utils/setRepo.hs
@@ -10,14 +10,10 @@ main = do
   (x:y:_) <- getArgs 
   func x y 
 
-f = func "https://github.com/xpika/templateify.git" "templateify" 
-
 func y' y = do
    x <- readFile y
    let pkgDesc = (\(ParseOk _ b) -> b) (parsePackageDescription x)
    let pkgDesc' = pkgDesc & packageDescriptionL . sourceReposL .~ [SourceRepo RepoHead (Just Git) (Just y') Nothing Nothing Nothing Nothing] -- Add repo
    let pkgDesc'' = pkgDesc' & packageDescriptionL . packageL . pkgVersionL . versionBranchL  . _last  %~ (+1) -- bump version
    let pkgDescString = showGenericPackageDescription pkgDesc''
-   -- writeFile x pkgDescString 
    putStrLn pkgDescString
-   return pkgDescString
