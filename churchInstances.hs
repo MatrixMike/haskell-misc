f6828c5599b02ddd0baa875e3fed9f6f8628945d
Alan Hawkins <alanpika_au@yahoo.com>
2017-01-19 21:11:44 +1100

    Update churchInstances.hs

--- churchInstances.hs
+++ churchInstances.hs
@@ -1,10 +1,15 @@
+
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes #-}
 
 import Data.Monoid
 
-type Church = forall r. (r -> r) -> (r -> r)
+
+main =print  [ (2 (+(1::Integer)) (1::Integer))::Integer
+   ,appEndo (appEndo 1 (Endo (+1))) 1
+  ]
+
 
 instance Num ((r -> r) -> r -> r) where
   m + n = \f x -> m f (n f x)
@@ -13,12 +18,14 @@ instance Num ((r -> r) -> r -> r) where
   fromInteger 1 = \f x -> f x
   fromInteger n = 1 + fromInteger (n-1)
 
+eg = appEndo (appEndo 1 (Endo (+1))) 1
+
 instance Num (Endo (Endo r)) where
+  fromInteger 0 = Endo (\(Endo f) -> (Endo (\x -> x)))
   fromInteger 1 = Endo (\(Endo f) -> (Endo (\x -> f x)))
-  {-
-  m + n = \f x -> m f (n f x)
-  m * n = \f -> m (n f)
-  fromInteger 0 = \f x -> x
   fromInteger n = 1 + fromInteger (n-1)
+  (Endo m) + (Endo n) = Endo (\(Endo f) -> (Endo (\x -> f x)))
+  {-
+  m * n = \f -> m (n f)
   -}
 
