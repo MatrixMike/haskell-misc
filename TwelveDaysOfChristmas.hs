0000e127307289d4bcc5cfd709315ed646b5042e
Mike J Hewitt <mikeh@electroteach.com>
2017-10-14 15:13:33 +1100

    after hindent and hlint

--- TwelveDaysOfChristmas.hs
+++ TwelveDaysOfChristmas.hs
@@ -2,24 +2,30 @@ module TwelveDaysOfChristmas where
 
 import Data.Tree
 
-gifts = [  
-   "Drummers Drumming"
-  ,"Pipers Piping"
-  ,"Lords-a-Leaping"
-  ,"Ladies Dancing"
-  ,"Maids-a-Milking"
-  ,"Swans-a-Swimming"
-  ,"Geese-a-Laying"
-  ,"Gold Rings"
-  ,"Colly Birds"
-  ,"French Hens"
-  ,"Turtle Doves"
-  ,"Partridge in a Pear Tree"
- ]
- 
+gifts =
+  [ "Drummers Drumming"
+  , "Pipers Piping"
+  , "Lords-a-Leaping"
+  , "Ladies Dancing"
+  , "Maids-a-Milking"
+  , "Swans-a-Swimming"
+  , "Geese-a-Laying"
+  , "Gold Rings"
+  , "Colly Birds"
+  , "French Hens"
+  , "Turtle Doves"
+  , "Partridge in a Pear Tree"
+  ]
 
 buildChristmasTree (gift:gifts) = Node gift (lowerGifts gifts)
- where lowerGifts [] = []
-       lowerGifts _ = [buildChristmasTree gifts]
- 
+  where
+    lowerGifts [] = []
+    lowerGifts _ = [buildChristmasTree gifts]
+
+{-
 main = putStrLn . drawTree . buildChristmasTree . map show . zip [1..] . reverse $ gifts
+-}
+main =
+  putStrLn .
+  drawTree . buildChristmasTree . zipWith (curry show) [1 ..] . reverse $
+  gifts
--- existential-qualification/eg2.hs
+++ existential-qualification/eg2.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}
 {-# LANGUAGE ExistentialQuantification #-} 
 
 import Data.Typeable
@@ -10,5 +11,6 @@ heteroList = [TB True,TB (2::Int),TB False,TB (),TB (4::Int)]
 isInteger :: (Typeable a) => a -> Bool
 isInteger n = typeOf n == typeOf (1::Int)
 
+main :: IO ()
 main = print $ length $ filter id $ map (\(TB x)-> isInteger x) heteroList
 
