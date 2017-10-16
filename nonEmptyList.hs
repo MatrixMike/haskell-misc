75f3287c058fc7a52a36e161aca8fe97039c296a
MatrixMike <matroxmike@icloud.com>
2017-10-16 19:44:27 +1100

    added type sigs

--- nonEmptyList.hs
+++ nonEmptyList.hs
@@ -1,13 +1,18 @@
-
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}
 data NonEmpty a = Cons a (NonEmpty a)
                 | End a
 
+testList:: NonEmpty Integer
 testList = Cons 1 (Cons 2 (Cons 3 (End 4)))
+
+testList2:: NonEmpty Integer
 testList2 = End 5
 
+nonEmptyHead:: NonEmpty p -> p
 nonEmptyHead (Cons a _) = a
 nonEmptyHead (End a) = a
 
+main :: IO()
 main = do 
    print (nonEmptyHead testList)
    print (nonEmptyHead testList2)
