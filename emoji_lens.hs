933f5d57b27304120eced18c9cb9405bd313221a
Mike J Hewitt <mikeh@electroteach.com>
2017-10-18 22:16:07 +1100

    cabal install xxx trick - see emoji

--- emoji_lens.hs
+++ emoji_lens.hs
@@ -1,28 +1,37 @@
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
-{-# Language NoMonomorphismRestriction #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
 import Control.Lens
 import Control.Monad.Trans.Class
-
  -- (🏋️ ) = lift
-(🔎 ) = (^.) -- get
-(🖌 ) = (.~) -- set
-(🔧 ) = (%~) -- modify
-(🏇 ) = traverse
 
-infixl 8🔧
+{-to permit compilation please do the following (takes a while)
+cabal install lens
+cabal repl
+:browse
+:load <this file>
+notice the output and cpmpare with the following 4 lines
+
+-}
+(🔎) = (^.) -- get
+
+(🖌) = (.~) -- set
+
+(🔧) = (%~) -- modify
+
+(🏇) = traverse
+
+infixl 8 🔧
 
 main = do
-  print $ (1,2) 🔎 _1
-  print $ (1,2) 🔎 _2
-  print $ (1,2) & (_1🖌 10)
-  print $ (1,2) & (_2🖌 10)
-  print $ (1,2) & (_1🔧 (*100))
-  print $ (1,2) & (_2🔧 (*100))
-  print $ [(1,2),(3,4)] & ( (🏇 ) . _1 🔧 (*1000))
-  print $ [(1,2),(3,4)] & ( (🏇 ) . _2 🔧 (*1000))
-
-
-
+  print $ (1, 2) 🔎 _1
+  print $ (1, 2) 🔎 _2
+  print $ (1, 2) & (_1 🖌 10)
+  print $ (1, 2) & (_2 🖌 10)
+  print $ (1, 2) & (_1 🔧 (* 100))
+  print $ (1, 2) & (_2 🔧 (* 100))
+  print $ [(1, 2), (3, 4)] & ((🏇) . _1 🔧 (* 1000))
+  print $ [(1, 2), (3, 4)] & ((🏇) . _2 🔧 (* 1000))
   -- outputs respectively
   -- 1
   -- 2
@@ -32,4 +41,3 @@ main = do
   -- (1,200)
   -- [(1000,2),(3000,4)]
   -- [(1,2000),(3,4000)]
-
--- x11-example1.hs
+++ x11-example1.hs
@@ -3,12 +3,6 @@
 import Control.Concurrent (threadDelay)
 import Control.Monad
 import Data.Bits
-
-{-
- ~/cabal/cabal install  X11
- after some chasing and digging the library below is made available by installing X11  ... 
- naming system here ?
--}
 import Graphics.X11.Xlib
 import Graphics.X11.Xlib.Extras
 import System.Exit (ExitCode(..), exitWith)
@@ -22,13 +16,7 @@ main = do
       scr = defaultScreenOfDisplay dpy
       visual = defaultVisualOfScreen scr
       attrmask = 0
-      x1 = 20
-      x2 = 150
-      y1 = 20
-      y2 = 150
   rw <- rootWindow dpy dflt
-  let winSizeX = 200
-  let winSizeY = 200
   win <-
     allocaSetWindowAttributes $ \attributes ->
       createWindow
@@ -36,8 +24,8 @@ main = do
         rw
         0
         0
-        winSizeX
-        winSizeY
+        200
+        200
         0
         (defaultDepthOfScreen scr)
         inputOutput
@@ -47,8 +35,7 @@ main = do
   setTextProperty dpy win "Hello my World" wM_NAME
   gc <- createGC dpy win
   mapWindow dpy win
-  forM_ (concat $repeat $ [1 .. 100] ++ [99,98 .. 2]) $ \x -- lambda see x below
-   -> do
+  forM_ (concat $repeat $ [1 .. 100] ++ [99,98 .. 2]) $ \x -> do
     threadDelay 10000
     clearWindow dpy win
     drawInWin x dpy win gc
@@ -59,33 +46,22 @@ main = do
  exitWith ExitSuccess
  -}
 
---    let x1 = 20
 drawInWin :: Integral a => a -> Display -> Drawable -> GC -> IO ()
 drawInWin x dpy win gc = do
   bgcolor <- initColor dpy "green"
   fgcolor <- initColor dpy "blue"
-  let x1 = 20
-      x2 = 150
-      y1 = 20
-      y2 = 150
-  fgcolor <- initColor dpy "red"
   setForeground dpy gc bgcolor
-  fillRectangle dpy win gc 0 0 100 100
+  fillRectangle dpy win gc 0 0 200 200
   setForeground dpy gc fgcolor
   fillRectangle dpy win gc (2 + fromIntegral x) 2 96 96
-  fgcolor <- initColor dpy "blue"
-  setForeground dpy gc fgcolor
- {-
- https://tronche.com/gui/x/xlib/graphics/drawing/XDrawLine.html
- http://www.mit.edu/afs.new/athena/system/i386_deb50/os-ubuntu-9.04/usr/athena/share/doc/ghc6-doc/libraries/x11/Graphics-X11-Xlib-Misc.html
- -}
-  drawLine dpy win gc x1 y1 x2 y2 -- x1 y1 x2 y2 
-  drawLine dpy win gc x1 y1 x2 (y2 - 10) -- x1 y1 x2 y2 
-  drawLine dpy win gc x1 y1 x2 (y2 - 20) -- x1 y1 x2 y2 
 
 initColor :: Display -> String -> IO Pixel
 initColor dpy color = do
   let colormap = defaultColormap dpy (defaultScreen dpy)
   (apros, real) <- allocNamedColor dpy colormap color
   return $ color_pixel apros
---initialiseLine 
+
+{-
+XDrawLine (display, d, gc, x1, y1, x2, y2)
+
+-}
