c11f2f5d2d29d480b8c9a9619e34739ff4a54fab
Mike J Hewitt <mikeh@electroteach.com>
2017-10-18 22:45:04 +1100

    after hindent - looks interesting

--- kmett_lens_examples/egs.hs
+++ kmett_lens_examples/egs.hs
@@ -1,17 +1,24 @@
-import Control.Lens
 import Control.Applicative
+import Control.Lens
 import Data.Char
 
-data Address = A { _road :: String , _city :: String , _postCode :: String} 
-	deriving (Show)
+data Address = A
+  { _road :: String
+  , _city :: String
+  , _postCode :: String
+  } deriving (Show)
 
-middleLens = lens  (\xs -> xs !! (length xs `div` 2)) (\xs n -> (\(h,ts) -> (h++ (set _head n) ts  ) ) (splitAt (length xs `div` 2) xs))
+middleLens =
+  lens
+    (\xs -> xs !! (length xs `div` 2))
+    (\xs n ->
+       (\(h, ts) -> (h ++ (set _head n) ts)) (splitAt (length xs `div` 2) xs))
 
-tuple3lens :: Traversal (a,a,a) (b,b,b) a b
-tuple3lens f (a,a',a'') = (,,) `fmap` f a <*> f a' <*> f a''
+tuple3lens :: Traversal (a, a, a) (b, b, b) a b
+tuple3lens f (a, a', a'') = (,,) `fmap` f a <*> f a' <*> f a''
 
 mapped4 :: Functor f => Setter (f a) (f b) a b
-mapped4 = sets (\xs -> fmap (xs)  )
+mapped4 = sets (\xs -> fmap (xs))
 
 addressTraversal :: Traversal Address Address String String
 addressTraversal f (A r c p) = (\r' -> A r' c p) <$> (f r)
@@ -19,16 +26,13 @@ addressTraversal f (A r c p) = (\r' -> A r' c p) <$> (f r)
 addressTraversal2 :: Traversal Address Address String String
 addressTraversal2 f (A r c p) = (\r' c' -> A r' c' p) <$> (f r) <*> (f c)
 
+alternateElements = traversed . indices even
 
-alternateElements = traversed. indices even
-
-
-main = do 
-	   mapM_ print [set middleLens 999 [1,2,3,4]]
-	   print $ over tuple3lens succ (1,2,3)
-	   print $ over mapped4 succ [1,2,3]
-	   let myAddress = A "Baker st" "Melbourne" "3000"
-	   print $ over addressTraversal (Prelude.map toUpper) myAddress
-	   print $ over addressTraversal2 (Prelude.map toUpper) myAddress
-	   print $ over alternateElements toUpper "mr studly speaking"
-	   
+main = do
+  mapM_ print [set middleLens 999 [1, 2, 3, 4]]
+  print $ over tuple3lens succ (1, 2, 3)
+  print $ over mapped4 succ [1, 2, 3]
+  let myAddress = A "Baker st" "Melbourne" "3000"
+  print $ over addressTraversal (Prelude.map toUpper) myAddress
+  print $ over addressTraversal2 (Prelude.map toUpper) myAddress
+  print $ over alternateElements toUpper "mr studly speaking"
--- kmett_lens_examples/state_example.hs
+++ kmett_lens_examples/state_example.hs
@@ -2,24 +2,26 @@ module StateGame where
 
 import Control.Lens
 import Control.Monad.State
- 
+
 type GameValue = Int
+
 type GameState = (Bool, Int)
- 
+
 playGame :: String -> State GameState GameValue
-playGame []     = do
-    (_, score) <- get
-    return score
- 
+playGame [] = do
+  (_, score) <- get
+  return score
 playGame (x:xs) = do
-    (on, score) <- get
-    case x of
-         'a' | on -> modify $ _2 +~ 1 
-         'b' | on -> modify $ _2 -~ 1 
-         'c' -> modify $ _1 %~ not
-         _ -> modify id
-    playGame xs
- 
+  (on, score) <- get
+  case x of
+    'a'
+      | on -> modify $ _2 +~ 1
+    'b'
+      | on -> modify $ _2 -~ 1
+    'c' -> modify $ _1 %~ not
+    _ -> modify id
+  playGame xs
+
 startState = (False, 0)
- 
+
 main = print $ evalState (playGame "abcaaacbbcabbab") startState
